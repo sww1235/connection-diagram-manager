217a1735a43c73403bc8f486832c02059ed31c10
Stephen Walker-Weinshenker <sww1235@sww1235.net>
2024-02-18 11:02:35 -0700

    Forgot to remove display impl from main datafile

--- crates/cdm_core/src/datatypes/file_types.rs
+++ crates/cdm_core/src/datatypes/file_types.rs
@@ -28,7 +28,6 @@ pub mod wire_cable;
 use log::trace;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::fmt;
 use std::fs::{self, File};
 use std::io;
 use std::path;
@@ -151,52 +150,4 @@ fn proj_dir_parse_inner(
     }
 
     Ok(())
-}
-
-impl fmt::Display for DataFile {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if let Some(wire_types) = &self.wire_types {
-            for (k, v) in wire_types {
-                writeln!(f, "Wire Type: {k}")?;
-                writeln!(f, "{v}")?;
-            }
-        }
-        if let Some(cable_types) = &self.cable_types {
-            for (k, v) in cable_types {
-                writeln!(f, "Cable Type: {k}")?;
-                writeln!(f, "{v}")?;
-            }
-        }
-        if let Some(term_cable_types) = &self.term_cable_types {
-            for (k, v) in term_cable_types {
-                writeln!(f, "Term Cable Type: {k}")?;
-                writeln!(f, "{v}")?;
-            }
-        }
-        if let Some(location_types) = &self.location_types {
-            for (k, v) in location_types {
-                writeln!(f, "Location Type: {k}")?;
-                writeln!(f, "{v}")?;
-            }
-        }
-        if let Some(connector_types) = &self.connector_types {
-            for (k, v) in connector_types {
-                writeln!(f, "Connector Type: {k}")?;
-                writeln!(f, "{v}")?;
-            }
-        }
-        if let Some(equipment_types) = &self.equipment_types {
-            for (k, v) in equipment_types {
-                writeln!(f, "Equipment Type: {k}")?;
-                writeln!(f, "{v}")?;
-            }
-        }
-        if let Some(pathway_types) = &self.pathway_types {
-            for (k, v) in pathway_types {
-                writeln!(f, "Pathway Type: {k}")?;
-                writeln!(f, "{v}")?;
-            }
-        }
-        Ok(())
-    }
 }
