f34dc494ded5e18f6d8d48e6c59f934d7bb7218c
Stephen Walker-Weinshenker <github@sww1235.net>
2025-09-09 21:33:14 -0600

    Started unit_helper structs

--- crates/cdm_core/src/datatypes.rs
+++ crates/cdm_core/src/datatypes.rs
@@ -1,15 +1,16 @@
+/// `color` is used to define common colors, as well as allow custom colors to be defined
+pub mod color;
 /// `file_types` contains all representations of data found in src and lib files found on disc.
 pub mod file_types;
-
 ///`internal_types` contains all representations of data used internally in the program.
 pub mod internal_types;
-
+/// `project` contains the types that define a project.
+pub mod project;
+/// `unit_helper` contains wrapper types around UOM units, to allow tracking what unit was defined
+/// in the data file easier.
+///
+/// Also contains conversion helper functions
+pub mod unit_helper;
 ///`util_types` contains utility types that are used in multiple other different types and files,
 ///including generic enums.
 pub mod util_types;
-
-/// `project` contains the types that define a project.
-pub mod project;
-
-/// `color` is used to define common colors, as well as allow custom colors to be defined
-pub mod color;
---
+++ crates/cdm_core/src/datatypes/unit_helper.rs
@@ -0,0 +1,41 @@
+use serde::{Deserialize, Serialize};
+use uom::si::rational64;
+
+/// Struct representing Area values
+#[derive(Debug, Default, PartialEq, Clone, Serialize, Deserialize)]
+pub struct Area {
+    pub value: rational64::Area,
+    unit_string: String,
+}
+
+/// Struct representing Cross Sectional Area values of conductors
+#[derive(Debug, Default, PartialEq, Clone, Serialize, Deserialize)]
+pub struct CrossSectionalArea {
+    pub value: rational64::Area,
+    unit_string: String,
+}
+
+/// Struct representing Electric Potential values
+#[derive(Debug, Default, PartialEq, Clone, Serialize, Deserialize)]
+pub struct ElectricPotential {
+    pub value: rational64::ElectricPotential,
+    unit_string: String,
+}
+
+/// Struct representing Length values
+#[derive(Debug, Default, PartialEq, Clone, Serialize, Deserialize)]
+pub struct Length {
+    pub value: rational64::Length,
+    unit_string: String,
+}
+
+/// Struct representing TemperatureInterval values
+#[derive(Debug, Default, PartialEq, Clone, Serialize, Deserialize)]
+pub struct TemperatureInterval {
+    pub value: rational64::TemperatureInterval,
+    unit_string: String,
+}
+
+//TODO: implement to/from CrossSectionalArea to AWG
+//
+//TODO: implement to/from Area to CrossSectionalArea
